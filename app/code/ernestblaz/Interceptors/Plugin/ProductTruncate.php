28cc9e8be4f726767f8f4a7452253dbb9cb8fd64
Blaz <ernest.blaz@accenture.com>
2021-01-25 12:02:24 +0100

    add interceptor around method

---
+++ app/code/ernestblaz/Interceptors/Plugin/ProductTruncate.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ernestblaz\Interceptors\Plugin;
+
+
+use _HumbugBoxe8a38a0636f4\Nette\Neon\Exception;
+use Magento\Framework\Exception\NoSuchEntityException;
+use function PHPUnit\Framework\throwException;
+
+class ProductTruncate
+{
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $_storeManager;
+
+    public function __construct(
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    ) {
+        $this->_storeManager = $storeManager;
+    }
+
+    public function aroundGetName(\Magento\Catalog\Model\Product $subject, callable $proceed)
+    {
+        $result = $proceed();
+
+        try {
+            if ($this->_storeManager->getStore()->getCode() == 'UK') {
+                $result = substr($result, 0, 15);
+            }
+        } catch (NoSuchEntityException $e) {
+            throwException($e);
+        }
+
+        return $result;
+    }
+}
