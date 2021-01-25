9c25c040b5f4735ceef53d17d53727fd12999298
Blaz <ernest.blaz@accenture.com>
2021-01-25 08:23:48 +0100

    overwrite Product class

---
+++ app/code/ernestblaz/Product/Model/Rewrite/Catalog/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ernestblaz\Product\Model\Rewrite\Catalog;
+
+
+class Product extends \Magento\Catalog\Model\Product
+{
+    /**
+     * Get product price through type instance
+     *
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        if ($this->_calculatePrice || !$this->getData(self::PRICE)) {
+            return $this->getPriceModel()->getPrice($this) * 1.1;
+        } else {
+            return $this->getData(self::PRICE) * 1.1;
+        }
+    }
+}
