5e9fe534b9af87dc0186b6a0128573e99c8041e3
Blaz <ernest.blaz@accenture.com>
2021-01-26 15:42:09 +0100

    add send reset password if tried to register with existing email

---
+++ app/code/ernestblaz/Events/Observer/ResetPassword.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ernestblaz\Events\Observer;
+
+use Magento\Customer\Model\AccountManagement;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Customer\Api\AccountManagementInterface;
+
+class ResetPassword implements ObserverInterface
+{
+    /**
+     * @var \Magento\Customer\Api\AccountManagementInterface
+     */
+    protected $customerAccountManagement;
+
+    public function __construct(
+        AccountManagementInterface $customerAccountManagement
+    ) {
+        $this->customerAccountManagement = $customerAccountManagement;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $this->customerAccountManagement->initiatePasswordReset(
+            $observer->getData('email'),
+            AccountManagement::EMAIL_RESET
+        );
+    }
+}
