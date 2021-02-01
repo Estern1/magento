930080e0852ab0044d057b8e1cd1e86234be7cc8
Blaz <ernest.blaz@accenture.com>
2021-02-01 15:48:28 +0100

    fix TemplateBlock

--- app/code/Ernestblaz/Blocks/Block/TemplateBlock.php
+++ app/code/Ernestblaz/Blocks/Block/TemplateBlock.php
@@ -6,18 +6,5 @@ namespace Ernestblaz\Blocks\Block;
 
 class TemplateBlock extends \Magento\Framework\View\Element\Template
 {
-    protected function _beforeToHtml()
-    {
-        $this->prepareBlockData();
-        return parent::_beforeToHtml();
-    }
-
-    protected function prepareBlockData()
-    {
-        $this->addData(
-            [
-                'description' => 'Opis testowy'
-            ]
-        );
-    }
+    
 }
