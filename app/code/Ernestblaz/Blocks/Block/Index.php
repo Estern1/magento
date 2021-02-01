35d6c2b90c767f2d317c99f80319ca53bb17dd07
Blaz <ernest.blaz@accenture.com>
2021-02-01 15:28:13 +0100

    ex. 1

---
+++ app/code/Ernestblaz/Blocks/Block/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ernestblaz\Blocks\Block;
+
+class Index extends \Magento\Framework\View\Element\AbstractBlock
+{
+    protected function _toHtml()
+    {
+        return 'Hello World!';
+    }
+}
