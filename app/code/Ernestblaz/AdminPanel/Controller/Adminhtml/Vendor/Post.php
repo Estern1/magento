e3c822e17e3c734be6792e39e093033d66c9d352
Blaz <ernest.blaz@accenture.com>
2021-02-12 17:36:35 +0100

    add validation if field 'is enabled' from config is enabled

--- app/code/Ernestblaz/AdminPanel/Controller/Adminhtml/Vendor/Post.php
+++ app/code/Ernestblaz/AdminPanel/Controller/Adminhtml/Vendor/Post.php
@@ -6,9 +6,24 @@ use Magento\Framework\Controller\ResultFactory;
 
 class Post extends \Magento\Backend\App\Action
 {
+    protected $helperData;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Ernestblaz\AdminPanel\Helper\Data $helperData
+    ) {
+        $this->helperData = $helperData;
+        return parent::__construct($context);
+    }
+
     public function execute()
     {
         $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+
+        if (!$this->helperData->getGeneralConfig('isenabled')) {
+            $resultPage->getConfig()->getTitle()->prepend('Vendor Post Form is not available');
+        }
+
         return $resultPage;
     }
 
